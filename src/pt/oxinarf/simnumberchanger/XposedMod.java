bdcec5469bb54fcf54eb4bc96066c406694bbf87
OXINARF <oxinarf@trashmail.net>
2013-05-28 01:10:53 +0100

    Small changes Make compatible with (Samsung phones with) Android 4.2.2

--- src/pt/oxinarf/simnumberchanger/XposedMod.java
+++ src/pt/oxinarf/simnumberchanger/XposedMod.java
@@ -2,87 +2,101 @@ package pt.oxinarf.simnumberchanger;
 
 import java.io.File;
 
-import android.app.AndroidAppHelper;
-import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Environment;
 import android.telephony.TelephonyManager;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XposedHelpers.ClassNotFoundError;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedMod implements IXposedHookLoadPackage {
-	
-	private static SharedPreferences prefs;
-	
-	private static final String MY_PACKAGE_NAME = XposedMod.class.getPackage().getName();
-	
-	private static Object phone;
-	
-	private static String number;
-	
-	private static TelephonyManager tm;
+
+	private final String MY_PACKAGE_NAME = XposedMod.class.getPackage().getName();
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (lpparam.packageName.equals("com.android.phone")) {
-			XposedHelpers.findAndHookMethod("com.android.phone.PhoneApp", lpparam.classLoader, "onCreate", new XC_MethodHook() {
+			Class<?> hookClass;
 
-				@Override
+			try {
+				hookClass = XposedHelpers.findClass("com.android.phone.PhoneGlobals", lpparam.classLoader);
+			}
+			catch (ClassNotFoundError e) {
+				try {
+					hookClass = XposedHelpers.findClass("com.android.phone.PhoneApp", lpparam.classLoader);
+				}
+				catch (ClassNotFoundError ex) {
+					XposedBridge.log("Classes don't exist - version not supported");
+					return;
+				}
+			}
+
+			File prefFile = new File(Environment.getDataDirectory(), "data/" + MY_PACKAGE_NAME + "/shared_prefs/" + "MainActivity.xml");
+
+			XSharedPreferences prefs;
+
+			if (prefFile.exists())
+				prefs = new XSharedPreferences(MY_PACKAGE_NAME, "MainActivity");
+			else
+				return;
+
+			final String number = prefs.getString("number", null);
+
+			if(number == null)
+				return;
+
+			XposedHelpers.findAndHookMethod(hookClass, "onCreate", new XC_MethodHook() {				@Override
 				protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-					File prefFile = new File(Environment.getDataDirectory(), "data/" + MY_PACKAGE_NAME + "/shared_prefs/" + "MainActivity.xml");
-					
-					if (prefFile.exists())
-						prefs = AndroidAppHelper.getSharedPreferencesForPackage(MY_PACKAGE_NAME, "MainActivity", Context.MODE_PRIVATE);
-					else
+					final Object phone;
+
+					try {
+						phone = XposedHelpers.getObjectField(param.thisObject, "phone");
+					}
+					catch (NoSuchFieldError ex) {
+						XposedBridge.log("Field 'phone' doesn't exist - version not supported");
 						return;
-					
-					number = prefs.getString("number", null);
-					
-					if(number == null)
-						return;
-					
-					phone = XposedHelpers.getObjectField(param.thisObject, "phone");
-					
+					}
+
 					if(phone == null) {
 						XposedBridge.log("Phone was null, it wasn't supposed!");
 						return;
 					}
-					
-					tm = (TelephonyManager) ((Application) param.thisObject).getSystemService("phone");
-					
+
+					final TelephonyManager tm = (TelephonyManager) ((Context) param.thisObject).getSystemService("phone");
+
 					new Thread(new Runnable() {
-				        public void run() {
-				        	
-				        	for(int simState = tm.getSimState(); simState != TelephonyManager.SIM_STATE_READY; simState = tm.getSimState()) {
-				        		try {
-				        			Thread.sleep(5000);
-				        		} catch (InterruptedException e) {}
-				        	}
+						public void run() {
 
-				        	String tag = (String) XposedHelpers.callMethod(phone, "getLine1AlphaTag");
-					
-				        	if(tag == null || tag.isEmpty() || tag.trim().equals("")) {
-				        		tag = "Line 1";
-				        		XposedBridge.log("Tag was empty - Line 1 now");
-				        	}
+							for(int simState = tm.getSimState(); simState != TelephonyManager.SIM_STATE_READY; simState = tm.getSimState()) {
+								try {
+									Thread.sleep(5000);
+								} catch (InterruptedException e) {}
+							}
 
-				        	String actualNumber = (String) XposedHelpers.callMethod(phone, "getLine1Number");
-					
-				        	XposedBridge.log("Actual Number is: " + actualNumber);
-					
-				        	if(number.equals(actualNumber))
-				        		return;
-					
-				        	XposedBridge.log("Going to write SIM number (" + number + ") now!");
-					
-				        	XposedHelpers.callMethod(phone, "setLine1Number", tag, number, null);
-				        }
+							String tag = (String) XposedHelpers.callMethod(phone, "getLine1AlphaTag");
+
+							if(tag == null || tag.isEmpty() || tag.trim().equals("")) {
+								tag = "Line 1";
+								XposedBridge.log("Tag was empty - 'Line 1' now");
+							}
+
+							String actualNumber = (String) XposedHelpers.callMethod(phone, "getLine1Number");
+
+							XposedBridge.log("Actual Number is: " + actualNumber);
+
+							if(number.equals(actualNumber))
+								return;
+
+							XposedBridge.log("Going to write SIM number (" + number + ") now!");
+
+							XposedHelpers.callMethod(phone, "setLine1Number", tag, number, null);
+						}
 					}).start();
-				}
+			}
 			});
 		}
 	}
